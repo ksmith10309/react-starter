15361bd981ec3bf138c51a8bc426edc74fde983f
Katherine Smith <katherine.lo.smith@gmail.com>
2018-11-27 15:52:00 -0800

    Complete React Starter

---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <h1>Hello World From Footer</h1>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Hello World From Header</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
